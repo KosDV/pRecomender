a8c801a33ae7cebc2bcf745d95398b145b7931dd
El Taha, Nadim <nadimeltaha@gmail.com>
2014-04-17 20:43:38 +0200

    Adds NewInstancesRoc.java and mades few changes

--- src/resources/Driver.java
+++ src/resources/Driver.java
@@ -10,67 +10,67 @@ import weka.core.Instance;
 import weka.core.Instances;
 
 public class Driver {
-    @SuppressWarnings({ "deprecation", "unchecked", "rawtypes" })
-    public static void main(String[] args) throws Exception{
+	@SuppressWarnings({ "deprecation", "unchecked", "rawtypes" })
+	public static void main(String[] args) throws Exception {
 
-        // Declare two numeric attributes
-        Attribute Attribute1 = new Attribute("firstNumeric");
-        Attribute Attribute2 = new Attribute("secondNumeric");
+		// Declare two numeric attributes
+		Attribute Attribute1 = new Attribute("firstNumeric");
+		Attribute Attribute2 = new Attribute("secondNumeric");
 
-        // Declare a nominal attribute along with its values
-        FastVector fvNominalVal = new FastVector(3);
-        fvNominalVal.addElement("blue");
-        fvNominalVal.addElement("gray");
-        fvNominalVal.addElement("black");
-        Attribute Attribute3 = new Attribute("aNominal", fvNominalVal);
+		// Declare a nominal attribute along with its values
+		FastVector fvNominalVal = new FastVector(3);
+		fvNominalVal.addElement("blue");
+		fvNominalVal.addElement("gray");
+		fvNominalVal.addElement("black");
+		Attribute Attribute3 = new Attribute("aNominal", fvNominalVal);
 
-        // Declare the class attribute along with its values
-        FastVector fvClassVal = new FastVector(2);
-        fvClassVal.addElement("positive");
-        fvClassVal.addElement("negative");
-        Attribute ClassAttribute = new Attribute("theClass", fvClassVal);
+		// Declare the class attribute along with its values
+		FastVector fvClassVal = new FastVector(2);
+		fvClassVal.addElement("positive");
+		fvClassVal.addElement("negative");
+		Attribute ClassAttribute = new Attribute("theClass", fvClassVal);
 
-        // Declare the feature vector
-        FastVector fvWekaAttributes = new FastVector(4);
-        fvWekaAttributes.addElement(Attribute1);   
-        fvWekaAttributes.addElement(Attribute2);   
-        fvWekaAttributes.addElement(Attribute3);   
-        fvWekaAttributes.addElement(ClassAttribute);
+		// Declare the feature vector
+		FastVector fvWekaAttributes = new FastVector(4);
+		fvWekaAttributes.addElement(Attribute1);
+		fvWekaAttributes.addElement(Attribute2);
+		fvWekaAttributes.addElement(Attribute3);
+		fvWekaAttributes.addElement(ClassAttribute);
 
-        // Create an empty training set
-        Instances isTrainingSet = new Instances("Rel", fvWekaAttributes, 10);      
+		// Create an empty training set
+		Instances isTrainingSet = new Instances("Rel", fvWekaAttributes, 10);
 
-        // Set class index
-        isTrainingSet.setClassIndex(3);
+		// Set class index
+		isTrainingSet.setClassIndex(3);
 
-        // Create the instance
-        Instance iExample = new DenseInstance(4);
-        iExample.setValue((Attribute)fvWekaAttributes.elementAt(0), 1.0);     
-        iExample.setValue((Attribute)fvWekaAttributes.elementAt(1), 0.5);     
-        iExample.setValue((Attribute)fvWekaAttributes.elementAt(2), "gray");
-        iExample.setValue((Attribute)fvWekaAttributes.elementAt(3), "positive");
+		// Create the instance
+		Instance iExample = new DenseInstance(4);
+		iExample.setValue((Attribute) fvWekaAttributes.elementAt(0), 1.0);
+		iExample.setValue((Attribute) fvWekaAttributes.elementAt(1), 0.5);
+		iExample.setValue((Attribute) fvWekaAttributes.elementAt(2), "gray");
+		iExample.setValue((Attribute) fvWekaAttributes.elementAt(3), "positive");
 
-        // add the instance
-        isTrainingSet.add(iExample);
-        Classifier cModel = (Classifier)new NaiveBayes();  
-        cModel.buildClassifier(isTrainingSet);
+		// add the instance
+		isTrainingSet.add(iExample);
+		Classifier cModel = (Classifier) new NaiveBayes();
+		cModel.buildClassifier(isTrainingSet);
 
-        // Test the model
-        Evaluation eTest = new Evaluation(isTrainingSet);
-        eTest.evaluateModel(cModel, isTrainingSet);
+		// Test the model
+		Evaluation eTest = new Evaluation(isTrainingSet);
+		eTest.evaluateModel(cModel, isTrainingSet);
 
-        // Print the result �� la Weka explorer:
-        String strSummary = eTest.toSummaryString();
-        System.out.println(strSummary);
+		// Print the result �� la Weka explorer:
+		String strSummary = eTest.toSummaryString();
+		System.out.println(strSummary);
 
-        // Get the confusion matrix
-        double[][] cmMatrix = eTest.confusionMatrix();
-        for(int row_i=0; row_i<cmMatrix.length; row_i++){
-            for(int col_i=0; col_i<cmMatrix.length; col_i++){
-                System.out.print(cmMatrix[row_i][col_i]);
-                System.out.print("|");
-            }
-            System.out.println();
-        }
-    }
+		// Get the confusion matrix
+		double[][] cmMatrix = eTest.confusionMatrix();
+		for (int row_i = 0; row_i < cmMatrix.length; row_i++) {
+			for (int col_i = 0; col_i < cmMatrix.length; col_i++) {
+				System.out.print(cmMatrix[row_i][col_i]);
+				System.out.print("|");
+			}
+			System.out.println();
+		}
+	}
 }
---
+++ src/resources/NewInstancesRoc.java
@@ -0,0 +1,41 @@
+package resources;
+
+import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+public class NewInstancesRoc {
+	public static void main(String[] args) throws Exception {
+		// load dataset
+		Instances data = DataSource.read("/Users/nadim/Workspace/weka-3-7-10/data/weather.nominal.arff");
+		// Constructor creating an empty set of instances. Copies references to
+		// the header information from the given set of instances.
+		Instances newData = null;
+		newData = new Instances(data, 1);
+		// Create empty instance with 5 attribute values
+		Instance inst = new DenseInstance(5);
+
+		// Set instance's dataset to be the dataset "race"
+		inst.setDataset(newData);
+
+		// Set instance's values for the attributes "length", "weight", and
+		// "position"
+		// @attribute outlook {sunny, overcast, rainy}
+		// @attribute temperature {hot, mild, cool}
+		// @attribute humidity {high, normal}
+		// @attribute windy {TRUE, FALSE}
+		// @attribute play {yes, no}
+
+		inst.setValue(0, "sunny");
+		inst.setValue(1, "hot");
+		inst.setValue(2, "high");
+		inst.setValue(3, "TRUE");
+
+		// Print the instance
+		System.out.println("The instance: " + inst);
+		// output on stdout
+		System.out.println(data);
+		System.out.println(newData);
+	}
+}
