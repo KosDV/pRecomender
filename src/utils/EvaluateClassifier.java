4abd977fdd0bf6fd7033f10f7c407a0f6f8db051
El Taha, Nadim <nadimeltaha@gmail.com>
2014-04-18 02:33:29 +0200

    Adds getConfusionMatrix implementation

--- src/utils/EvaluateClassifier.java
+++ src/utils/EvaluateClassifier.java
@@ -8,31 +8,38 @@ import weka.classifiers.evaluation.Evaluation;
 
 public class EvaluateClassifier implements EvaluateClassifierInterface {
 
-    @Override
-    public Evaluation crossValidation(Classifier classifier, Instances data,
-            int numFolds, int numSeeds) throws Exception {
+	@Override
+	public Evaluation crossValidation(Classifier classifier, Instances data,
+			int numFolds, int numSeeds) throws Exception {
 
-        Evaluation eval = new Evaluation(data);
-        Random rand = new Random(numSeeds);
+		Evaluation eval = new Evaluation(data);
+		Random rand = new Random(numSeeds);
 
-        eval.crossValidateModel(classifier, data, numFolds, rand);
+		eval.crossValidateModel(classifier, data, numFolds, rand);
 
-        System.out.println(eval.toClassDetailsString());
-        
-        return eval;
+		System.out.println(eval.toClassDetailsString());
 
-    }
+		return eval;
 
-    @Override
-    public double[][] getConfusionMatrix(Evaluation eval) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	}
 
-    @Override
-    public double getKappa(Evaluation eval) {
-        // TODO Auto-generated method stub
-        return 0;
-    }
+	@Override
+	public double[][] getConfusionMatrix(Evaluation eval) {
+		double[][] cnMatrix = eval.confusionMatrix();
+		for (int row_i = 0; row_i < cnMatrix.length; row_i++) {
+			for (int col_i = 0; col_i < cnMatrix.length; col_i++) {
+				System.out.print(cnMatrix[row_i][col_i]);
+				System.out.print("|");
+			}
+			System.out.println();
+		}
+		return cnMatrix;
+	}
+
+	@Override
+	public double getKappa(Evaluation eval) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
