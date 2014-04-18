35521ae15b13437fe1f640c1e7ebe501e8785748
El Taha, Nadim <nadimeltaha@gmail.com>
2014-04-18 13:22:20 +0200

    Adds plotCurveROC implementation

--- src/utils/EvaluateClassifier.java
+++ src/utils/EvaluateClassifier.java
@@ -1,11 +1,22 @@
 package utils;
 
+import java.awt.BorderLayout;
 import java.util.Random;
 
-import weka.classifiers.Classifier;
-import weka.core.Instances;
-import weka.classifiers.evaluation.Evaluation;
+import javax.swing.JFrame;
 
+import weka.classifiers.Classifier;
+import weka.classifiers.evaluation.Evaluation;
+import weka.classifiers.evaluation.ThresholdCurve;
+import weka.core.Instances;
+import weka.core.Utils;
+import weka.gui.visualize.PlotData2D;
+import weka.gui.visualize.ThresholdVisualizePanel;
+
+/**
+ * Business Logic implementation of Classifier Evaluation
+ *
+ */
 public class EvaluateClassifier implements EvaluateClassifierInterface {
 
 	@Override
@@ -40,6 +51,43 @@ public class EvaluateClassifier implements EvaluateClassifierInterface {
 	public double getKappa(Evaluation eval) {
 		double kappaIndex = eval.kappa();
 		return kappaIndex;
+	}
+
+	@Override
+	public void plotCurveROC(Evaluation eval) {
+		// Generate the curve
+		ThresholdCurve tc = new ThresholdCurve();
+		Instances curve = tc.getCurve(eval.predictions());
+
+		// Plot the curve
+		ThresholdVisualizePanel tvp = new ThresholdVisualizePanel();
+		tvp.setROCString("(Area under ROC = "
+				+ Utils.doubleToString(ThresholdCurve.getROCArea(curve), 4)
+				+ ")");
+		tvp.setName(curve.relationName());
+		PlotData2D plotData = new PlotData2D(curve);
+		plotData.setPlotName(curve.relationName());
+		plotData.addInstanceNumberAttribute();
+		// Specifying the number of connected points, all
+		boolean[] cp = new boolean[curve.numInstances()];
+		for (int i = 0; i < cp.length; i++) {
+			cp[i] = true;
+		}
+		try {
+			plotData.setConnectPoints(cp);
+			tvp.addPlot(plotData);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// Display curve
+		final JFrame jf = new JFrame("WEKA ROC: " + tvp.getName());
+		jf.setSize(500, 400);
+		jf.getContentPane().setLayout(new BorderLayout());
+		jf.getContentPane().add(tvp, BorderLayout.CENTER);
+		jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		jf.setVisible(true);
+
 	}
 
 }
--- src/utils/EvaluateClassifierInterface.java
+++ src/utils/EvaluateClassifierInterface.java
@@ -5,31 +5,38 @@ import weka.classifiers.evaluation.Evaluation;
 import weka.core.Instances;
 
 public interface EvaluateClassifierInterface {
-    /**
-     * Performs a n-Fold-crossValidation of a Classifier using the given
-     * Instances
-     * 
-     * @param classifier
-     * @param data
-     * @param numFolds, recommended numFolds = 10;
-     * @param numSeeds, i.e numSeeds = 1;
-     * @return the evaluated Evaluation
-     * @throws Exception
-     */
-    public Evaluation crossValidation(Classifier classifier, Instances data,
-            int numFolds, int numSeeds) throws Exception;
+	/**
+	 * Performs a n-Fold-crossValidation of a Classifier using the given
+	 * Instances
+	 * 
+	 * @param classifier
+	 * @param data
+	 * @param numFolds, recommended numFolds = 10;
+	 * @param numSeeds, i.e numSeeds = 1;
+	 * @return evaluated Evaluation
+	 * @throws Exception
+	 */
+	public Evaluation crossValidation(Classifier classifier, Instances data,
+			int numFolds, int numSeeds) throws Exception;
 
-    /**
-     * 
-     * @param eval
-     * @return a copy of the confusion matrix as a two-dimensional array
-     */
-    public double[][] getConfusionMatrix(Evaluation eval);
+	/**
+	 * 
+	 * @param evaluated Evaluation
+	 * @return a copy of the confusion matrix as a two-dimensional array
+	 */
+	public double[][] getConfusionMatrix(Evaluation eval);
 
-    /**
-     * 
-     * @param eval
-     * @return the value of kappa statistic if class is nominal
-     */
-    public double getKappa(Evaluation eval);
+	/**
+	 * 
+	 * @param evaluated Evaluation
+	 * @return value of Kappa statistic if class is nominal
+	 */
+	public double getKappa(Evaluation eval);
+
+	/**
+	 * based pretty much in FracPete (fracpete at waikato dot ac dot nz) code
+	 * 
+	 * @param evaluated Evaluation
+	 */
+	public void plotCurveROC(Evaluation eval);
 }
