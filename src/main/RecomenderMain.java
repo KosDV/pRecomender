760ddbd28a83d20d66a90f0b34450f91876f12ac
El Taha, Nadim <nadimeltaha@gmail.com>
2014-05-27 16:52:40 +0200

    Fixes prediction and adds cl input

--- src/main/RecomenderMain.java
+++ src/main/RecomenderMain.java
@@ -1,5 +1,9 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import weka.classifiers.Classifier;
 import weka.classifiers.functions.VotedPerceptron;
 import weka.core.DenseInstance;
@@ -11,6 +15,11 @@ public class RecomenderMain {
 
 	public static void predict(String name, Integer total_nominations,
 			String category_next, String awarded_next) throws Exception {
+		System.out.println("\n\n");
+		System.out.println("*****************");
+		System.out.println("* TRAINING DATA *");
+		System.out.println("*****************");
+
 		// load training data
 		Instances data = DataSource.read("oscars.training.arff");
 		data.setClassIndex(data.numAttributes() - 1);
@@ -18,31 +27,37 @@ public class RecomenderMain {
 		// train classifier
 		Classifier cl = new VotedPerceptron();
 		cl.buildClassifier(data);
+		// output on stdout
+		System.out.println(data);
 
 		// create new set of data
 		Instances newData = new Instances(data, 1);
 		Instance inst = new DenseInstance(5);
 		inst.setDataset(newData);
-
-		// output on stdout
-		System.out.println(data);
+		Thread.sleep(1000);
+		System.out.println("\n\n");
+		System.out.println("*****************");
+		System.out.println("*   TEST DATA   *");
+		System.out.println("*****************");
 		System.out.println(newData);
-
-		System.out.println("Training finished!\n");
 
 		// add attribute values
 		inst.setValue(0, name);
 		inst.setValue(1, total_nominations);
 		inst.setValue(2, category_next);
 		inst.setValue(3, awarded_next);
+		System.out.println(inst);
 
 		// predict class
-		Double pred = cl.classifyInstance(inst);
-		Double classValue = inst.classValue();
-		System.out.println(inst);
-		System.out.println(classValue + " -> " + pred);
-
-		System.out.println("Predicting finished!");
+		double pred = cl.classifyInstance(inst);
+		Thread.sleep(1000);
+		System.out.println("\n\n");
+		System.out.println("*****************");
+		System.out.println("*    RESULTS    *");
+		System.out.println("*****************");
+		// get prediction
+		System.out.println(inst.stringValue(0) + " will repeat nomination? "
+				+ data.classAttribute().value((int) pred).toUpperCase());
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -53,12 +68,32 @@ public class RecomenderMain {
 		String name, category_next, awarded_next;
 		Integer total_nominations;
 
-		name = "Jeff-Bridges";
-		total_nominations = 6;
-		category_next = "bestActor";
-		awarded_next = "no";
+		// name = "Jeff-Bridges";
+		// total_nominations = 6;
+		// category_next = "bestActor";
+		// awarded_next = "no";
+		try {
+			BufferedReader bufferRead = new BufferedReader(
+					new InputStreamReader(System.in));
+			System.out.println("*****************");
+			System.out.println("*    WELCOME    *");
+			System.out.println("*****************");
+			System.out
+					.println("Please, introduces the following parameters...");
+			System.out.println("Actor");
+			name = bufferRead.readLine();
+			System.out.println("Total nominations");
+			total_nominations = Integer.parseInt(bufferRead.readLine());
+			System.out.println("Category next");
+			category_next = bufferRead.readLine();
+			System.out.println("Awarded next");
+			awarded_next = bufferRead.readLine();
 
-		predict(name, total_nominations, category_next, awarded_next);
+			predict(name, total_nominations, category_next, awarded_next);
+
+		} catch (IOException e) {
+			e.getMessage();
+		}
 
 	}
 
